cff0b19866bca572009aca70ea4380f3f3ad9ba7
Okami Chen <sync402@gmail.com>
2024-08-09 02:25:38 -0400

    * upgrade

--- conf/octane/start.sh
+++ conf/octane/start.sh
@@ -10,83 +10,9 @@ fi
 cat /usr/local/etc/php/php.ini-production > /usr/local/etc/php/php.ini
 cat /run/nginx/nginx.conf > /etc/nginx/nginx.conf
 
-# For Laravel
-if [ ! -d "/home/wwwlogs" ];then
-    mkdir /home/wwwlogs
-fi
-
-chown www-data:www-data /home/wwwlogs
-
-if [ ! -d "/var/www/html/storage" ];then
-    mkdir /var/www/html/storage
-fi
-
-if [ ! -d "/var/www/html/storage/logs" ];then
-    mkdir /var/www/html/storage/logs
-fi
-
-if [ ! -d "/var/www/html/storage/app" ];then
-    mkdir /var/www/html/storage/app
-fi
-
-if [ ! -d "/var/www/html/storage/framework" ];then
-    mkdir /var/www/html/storage/framework
-fi
-
-if [ ! -d "/var/www/html/storage/logs/supervisor" ];then
-  mkdir -p /var/www/html/storage/logs/supervisor
-fi
-
-if [ ! -f "/var/www/html/storage/logs/supervisor/app_out.log" ];then
-    touch /var/www/html/storage/logs/supervisor/app_out.log
-fi
-
-if [ ! -f "/var/www/html/storage/logs/supervisor/app_out.log" ];then
-    touch /var/www/html/storage/logs/supervisor/app_err.log
-fi
-
-chmod 777 -R /var/www/html/storage
-
-if [ ! -d "/var/www/html/bootstrap/cache" ];then
-  mkdir -p /var/www/html/bootstrap/cache
-fi
-
-if [ -d "/var/www/html/bootstrap/cache" ];then
-    chmod 777 -R /var/www/html/bootstrap/cache
-fi
-
-if [ -f "/var/www/html/artisan" ];then
-    chmod +x /var/www/html/artisan
-fi
-
-# For System
-echo "COLUMNS=2000" >> /root/.bashrc
-echo 'alias ll="ls -la"' >> /root/.bashrc
-
-echo -e "\033[42;37m Check Opcache \033[0m"
-
-if [ ${OPCACHE_ENABLE} == 0 ];then
-  sed -i 's/opcache.enable = 1/opcache.enable = 0/g' /usr/local/etc/php/conf.d/docker-php-ext-zzz-opcache.ini
-fi
-
-if [ -f "/var/www/html/storage/logs/octane-server-state.json" ];then
-    rm -rf /var/www/html/storage/logs/octane-server-state.json
-fi
-
-# For Supervisor
-
-if [ -d "/var/www/html/supervisor.d" ];then
-  echo -e "\033[42;37m copy /var/www/html/supervisor.d \033[0m"
-  cp /var/www/html/supervisor.d/*.ini /etc/supervisor.d
-fi
-
 if [ -f "/opt/runtime.sh" ];then
   echo -e "\033[42;37m exec /opt/runtime.sh \033[0m"
   /bin/bash /opt/runtime.sh
-fi
-
-if [ -f "/opt/supervisor.sh" ];then
-  /bin/bash /opt/supervisor.sh
 fi
 
 /usr/bin/supervisord -c /etc/supervisord.conf
--- conf/shell/runtime.sh
+++ conf/shell/runtime.sh
@@ -1,4 +1,68 @@
 #!/usr/bin/env bash
+# For System
+echo "COLUMNS=2000" >> /root/.bashrc
+echo 'alias ll="ls -la"' >> /root/.bashrc
+
+if [ ! -d "/home/wwwlogs" ];then
+    mkdir /home/wwwlogs
+fi
+# For Laravel
+
+if [ -f "/var/www/html/artisan" ];then
+
+  chmod +x /var/www/html/artisan
+
+  chown www-data:www-data /home/wwwlogs
+
+  if [ ! -d "/var/www/html/storage" ];then
+      mkdir /var/www/html/storage
+  fi
+
+  if [ ! -d "/var/www/html/storage/logs" ];then
+      mkdir /var/www/html/storage/logs
+  fi
+
+  if [ ! -d "/var/www/html/storage/app" ];then
+      mkdir /var/www/html/storage/app
+  fi
+
+  if [ ! -d "/var/www/html/storage/framework" ];then
+      mkdir /var/www/html/storage/framework
+  fi
+
+  if [ ! -d "/var/www/html/storage/logs/supervisor" ];then
+    mkdir -p /var/www/html/storage/logs/supervisor
+  fi
+
+  if [ ! -f "/var/www/html/storage/logs/supervisor/app_out.log" ];then
+      touch /var/www/html/storage/logs/supervisor/app_out.log
+  fi
+
+  if [ ! -f "/var/www/html/storage/logs/supervisor/app_out.log" ];then
+      touch /var/www/html/storage/logs/supervisor/app_err.log
+  fi
+
+  chmod 777 -R /var/www/html/storage
+
+  if [ ! -d "/var/www/html/bootstrap/cache" ];then
+    mkdir -p /var/www/html/bootstrap/cache
+  fi
+
+  if [ -d "/var/www/html/bootstrap/cache" ];then
+      chmod 777 -R /var/www/html/bootstrap/cache
+  fi
+
+fi
+
+echo -e "\033[42;37m Check Opcache \033[0m"
+
+if [ ${OPCACHE_ENABLE} == 0 ];then
+  sed -i 's/opcache.enable = 1/opcache.enable = 0/g' /usr/local/etc/php/conf.d/docker-php-ext-zzz-opcache.ini
+fi
+
+if [ -f "/var/www/html/storage/logs/octane-server-state.json" ];then
+    rm -rf /var/www/html/storage/logs/octane-server-state.json
+fi
 
 echo -e "\033[42;37m Check /var/www/html/.env.${DOCKER_ENV} \033[0m"
 
@@ -7,16 +71,22 @@ if [ -f "/var/www/html/.env.${DOCKER_ENV}" ];then
     echo -e "\033[42;37m cat .env.${DOCKER_ENV} ->  .env \033[0m"
 fi
 
-echo -e "\033[42;37m Check /var/www/html/supervisor.d/${DOCKER_ENV} \033[0m"
+# For Supervisord
+
+echo -e "\033[42;37m Check /var/www/html/supervisor.d \033[0m"
 
 if [ -d /var/www/html/supervisor.d/ ];then
     cp /var/www/html/supervisor.d/*.ini /etc/supervisor.d
 fi
 
+echo -e "\033[42;37m Check /var/www/html/supervisor.d/${DOCKER_ENV} \033[0m"
+
 if [ -d "/var/www/html/supervisor.d/${DOCKER_ENV}" ];then
     cp /var/www/html/supervisor.d/${DOCKER_ENV}/*.ini /etc/supervisor.d
     echo -e "\033[42;37m copy supervisor.d/${DOCKER_ENV}/*.ini To  /etc/supervisor.d \033[0m"
 fi
+
+# For Composer
 
 echo -e "\033[42;37m Check /var/www/html/composer.${DOCKER_ENV}.json \033[0m"
 
@@ -41,7 +111,7 @@ fi
 # For Nginx
 if [ -f "/usr/local/etc/php-fpm.d/www.conf" ];then
 
-    echo -e "\033[42;37m replace php-fpm \033[0m"
+    echo -e "\033[42;37m Replace php-fpm \033[0m"
     # pm.max_children：静态方式下开启的php-fpm进程数量
     # pm.max_requests：php-fpm子进程能处理的最大请求数
 
